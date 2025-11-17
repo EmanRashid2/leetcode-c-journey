07f0b679c412e9b6a32e070b4dfa5e90b2172258
EmanRashid <emansiddiqui2781@gmail.com>
2025-11-17 22:20:57 +0500

    Add files via upload
    A simple C program to find indices of two numbers in an array that add up to a given target. Implements a brute-force approach using nested loops. Prints the indices of the pair if found.

---
+++ two sum.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    static int result[2];
+    *returnSize = 2;
+
+    for (int i = 0; i < numsSize; i++) {
+        for (int j = i + 1; j < numsSize; j++) {
+            if (nums[i] + nums[j] == target) {
+                result[0] = i;
+                result[1] = j;
+                return result;
+            }
+        }
+    }
+    return NULL;
+}
+
+int main() {
+    int nums[] = {2, 7, 11, 15};
+    int target = 9;
+    int returnSize;
+
+    int* ans = twoSum(nums, 4, target, &returnSize);
+
+    printf("[%d, %d]\n", ans[0], ans[1]);
+
+    return 0;
+}
